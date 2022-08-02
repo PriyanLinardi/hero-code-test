80d1c693a44a31f5e544835302b193453992be0a
PriyanLinardi <96510386+PriyanLinardi@users.noreply.github.com>
2022-08-02 12:13:33 +0700

    Add files via upload

---
+++ Travlr/Travlr/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Travlr.Models;
+using Travlr.Models.Repository;
+using Travlr.Services;
+
+namespace Travlr.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            ProductRepository productRepos = new ProductRepository();
+            List<SelectListItem> productItems = productRepos.GetSelectListItem();
+            ViewBag.ProductList = productItems;
+
+            return View();
+        }
+    }
+}
---
+++ Travlr/Travlr/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Travlr.Models;
+using Travlr.Models.Repository;
+
+namespace Travlr.Controllers
+{
+    public class ProductController : Controller
+    {
+        [HttpPost]
+        public int GetPrice(int productID, bool isDiscount)
+        {
+            try
+            {
+                ProductRepository productRepos = new ProductRepository();
+                Product product = productRepos.GetProductById(productID);
+                return product.advertisedPrice - (isDiscount ? product.commission / 2 : 0);
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+}
