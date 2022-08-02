8c1f45cb6a490548199a0b434010a1661d359789
PriyanLinardi <96510386+PriyanLinardi@users.noreply.github.com>
2022-08-02 12:05:34 +0700

    Add files via upload

---
+++ Travlr/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Travlr.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
