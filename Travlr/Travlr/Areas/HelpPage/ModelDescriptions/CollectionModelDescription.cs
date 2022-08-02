e1d67a21efa198c191cfdc0f5efafc18a1dfc01c
PriyanLinardi <96510386+PriyanLinardi@users.noreply.github.com>
2022-08-02 12:08:30 +0700

    Add files via upload

---
+++ Travlr/Travlr/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Travlr.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Travlr/Travlr/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Travlr.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
