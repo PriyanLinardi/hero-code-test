7cbc55797b2df8499655d4e7f19cc37962ea6569
PriyanLinardi <96510386+PriyanLinardi@users.noreply.github.com>
2022-08-02 12:06:59 +0700

    Delete Travlr/Areas/HelpPage directory

--- Travlr/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Travlr.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Travlr/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Travlr.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
